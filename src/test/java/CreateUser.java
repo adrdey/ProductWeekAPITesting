d4aa26b2d0bce4c8fc9d84ffbba5a91adaf3dbad
Adrito Dey <adrdey@deloitte.com>
2021-12-23 12:33:58 +0530

    Added Initial API structure

---
+++ src/test/java/CreateUser.java
@@ -0,0 +1,87 @@
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import static io.restassured.RestAssured.given;
+
+
+public class CreateUser {
+    RequestSpecification requestSpecification;
+    ResponseSpecification responseSpecification;
+    RequestSpecBuilder requestSpecBuilder;
+    ResponseSpecBuilder responseSpecBuilder;
+    Properties properties = new Properties();
+    FileInputStream fileinputstream = new FileInputStream("src/test/TestResources/Data.Properties");
+    JSONArray userResponseJSONArray;
+    Response userResponse;
+    //ExtentReports
+    static ExtentHtmlReporter htmlReporter;
+    static ExtentReports extent;
+
+    public CreateUser() throws FileNotFoundException {
+    }
+
+    @BeforeTest
+    public void setupRequestResponseSpecifications() throws IOException {
+properties.load(fileinputstream);
+        requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecBuilder.setBaseUri(properties.getProperty("baseUri")).
+                addHeader("Content-Type", "application/json").log(LogDetail.ALL);
+        requestSpecification = requestSpecBuilder.build();
+        responseSpecBuilder = new ResponseSpecBuilder().
+                expectContentType(ContentType.JSON).
+                expectStatusCode(200).
+                expectHeader("Content-Type", "application/json");
+        responseSpecification = responseSpecBuilder.build();
+
+//        htmlReporter = new ExtentHtmlReporter("UserAPITestExtentReport.html");
+//        extent = new ExtentReports();
+//        extent.attachReporter(htmlReporter);
+
+    }
+
+    @Test(priority = 0)
+    public void specificationsValidations() {
+        //validating t
+        File postJson= new File("C:\\Users\\adrdey\\IdeaProjects\\ProductWeek_API_Assignment\\src\\test\\TestResources\\CreateUserData.json");
+        userResponse=
+                given().spec(requestSpecification).
+                        body(postJson).
+                        when().post(properties.getProperty("userUri")).
+                        then().spec(responseSpecification).log().all().extract().response();
+        JSONObject userResponseJSON = new JSONObject(userResponse.asString());
+
+        System.out.println(userResponseJSON);
+
+//        ExtentTest test = extent.createTest("Specification Validations", "Checking the Status Code and the" +
+//                "request and response specifications prior to all others tests.");
+//        test.log(Status.INFO, "Validating Specifications.");
+//        test.info("The checking of the Request and Response Specification is done before other tests. The validations are complete.");
+
+    }
+
+
+    @AfterTest
+    public void windUp() {
+        //extent.flush();
+    }
+
+
+}
---
+++ src/test/java/GetUser.java
@@ -0,0 +1,182 @@
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Properties;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class GetUser {
+    RequestSpecification requestSpecification;
+    ResponseSpecification responseSpecification;
+    RequestSpecBuilder requestSpecBuilder;
+    ResponseSpecBuilder responseSpecBuilder;
+   Properties properties = new Properties();
+   FileInputStream fileinputstream = new FileInputStream("src/test/TestResources/Data.Properties");
+    JSONArray userResponseJSONArray;
+    Response userResponse;
+    //ExtentReports
+    static ExtentHtmlReporter htmlReporter;
+    static ExtentReports extent;
+
+    public GetUser() throws FileNotFoundException {
+    }
+
+    @BeforeTest
+    public void setupRequestResponseSpecifications() throws IOException {
+        properties.load(fileinputstream);
+        requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecBuilder.setBaseUri(properties.getProperty("baseUri")).
+                addHeader("Content-Type", "application/json").log(LogDetail.ALL);
+        requestSpecification = requestSpecBuilder.build();
+        responseSpecBuilder = new ResponseSpecBuilder().
+                expectContentType(ContentType.JSON).
+                expectStatusCode(200).
+                expectHeader("Content-Type", "application/json");
+        responseSpecification = responseSpecBuilder.build();
+
+//        htmlReporter = new ExtentHtmlReporter("UserAPITestExtentReport.html");
+//        extent = new ExtentReports();
+//        extent.attachReporter(htmlReporter);
+
+    }
+
+    @Test(priority = 0)
+    public void specificationsValidations() {
+        //validating t
+        userResponse=
+                given().spec(requestSpecification).
+                        when().get(properties.getProperty("userUrl")).
+                        then().spec(responseSpecification).log().all().extract().response();
+        userResponseJSONArray = new JSONArray(userResponse.asString());
+
+        System.out.println(this.userResponseJSONArray);
+
+//        ExtentTest test = extent.createTest("Specification Validations", "Checking the Status Code and the" +
+//                "request and response specifications prior to all others tests.");
+//        test.log(Status.INFO, "Validating Specifications.");
+//        test.info("The checking of the Request and Response Specification is done before other tests. The validations are complete.");
+
+    }
+
+    @Test(
+            priority = 1
+    )
+    public void UsersandEmailPresentValidation() {
+        boolean isIDorEmailNull = true;
+        for(int i = 0; i < this.userResponseJSONArray.length(); ++i) {
+
+            JSONObject userJSONObject = this.userResponseJSONArray.getJSONObject(i);
+            if(userJSONObject.get("username") == null || userJSONObject.get("email") == null){
+
+                isIDorEmailNull = false;
+                break;
+            }
+        }
+
+
+        Assert.assertTrue(isIDorEmailNull);
+
+    }
+
+    @Test(
+            priority = 2
+    )
+    public void UniqueIDpresent() {
+
+        HashSet<String>userid = new HashSet<>();
+        int Number_of_Entries = this.userResponseJSONArray.length();
+
+        for(int i = 0; i < this.userResponseJSONArray.length(); ++i) {
+            JSONObject userJSONObject = this.userResponseJSONArray.getJSONObject(i);
+            userid.add(userJSONObject.get("id").toString());
+        }
+
+
+        Assert.assertEquals(userid.size() , Number_of_Entries);
+    }
+
+    @Test(
+            priority = 3
+    )
+    public void userPasswordValidation() {
+        boolean allPasswordsAreValid = true;
+
+        for(int i = 0; i < this.userResponseJSONArray.length(); ++i) {
+            JSONObject userJSONObject = this.userResponseJSONArray.getJSONObject(i);
+            Object userPassword = userJSONObject.get("password");
+            allPasswordsAreValid = this.validatePassword(String.valueOf(userPassword));
+            if (!allPasswordsAreValid) {
+                System.out.println("Object that failed.");
+                System.out.println(userJSONObject.get("username"));
+                System.out.println(userPassword);
+                break;
+            }
+        }
+
+        MatcherAssert.assertThat(allPasswordsAreValid, Matchers.is(Matchers.equalTo(true)));
+//        ExtentTest test = extent.createTest("Password Validations", "Checking one letter, one digit and one special character in the password.");
+//        test.log(Status.INFO, "Validating Password.");
+//        test.info("We validate all the password for the presence of one letter, one digit or one special character.");
+    }
+
+    @AfterTest
+    public void windUp() {
+        //extent.flush();
+    }
+
+    public boolean validatePassword(String password) {
+        boolean valid = false;
+        int numberCount = 0;
+        int specialCharacterCount = 0;
+        int characterCount = 0;
+        HashSet<Character> specialCharacters = new HashSet();
+        specialCharacters.add('@');
+        specialCharacters.add('!');
+        specialCharacters.add('#');
+        specialCharacters.add('$');
+        specialCharacters.add('%');
+        specialCharacters.add('^');
+        specialCharacters.add('&');
+        specialCharacters.add('_');
+        specialCharacters.add('*');
+        char[] var7 = password.toCharArray();
+        int var8 = var7.length;
+
+        for(int var9 = 0; var9 < var8; ++var9) {
+            char c = var7[var9];
+            if (Character.isDigit(c)) {
+                ++numberCount;
+            } else if (Character.isAlphabetic(c)) {
+                ++characterCount;
+            } else if (specialCharacters.contains(c)) {
+                ++specialCharacterCount;
+            }
+        }
+
+        if (numberCount >= 1 || characterCount >= 1 || specialCharacterCount >= 1) {
+            valid = true;
+        }
+
+        return valid;
+    }
+}
